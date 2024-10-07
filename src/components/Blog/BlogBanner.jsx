bbf0aeff86dbfea9d2a7ec56b902229d61e557ab
alzinan1234 <pappurohan1100@gmail.com>
2024-10-07 14:52:41 +0600

    added

--- src/components/Blog/BlogBanner.jsx
+++ src/components/Blog/BlogBanner.jsx
@@ -1,7 +1,5 @@
-import blogBanner from "../../image/blog/page-title-2.464b018b.jpg";
+import blogBanner from "../../image/blog/contact with us (3).png";
 import Image from "next/image";
-import Link from "next/link";
-import { FaCircleDot } from "react-icons/fa6";
 
 const BlogBanner = () => {
   return (
@@ -10,26 +8,13 @@ const BlogBanner = () => {
         {/* Image container */}
         <div className="w-full z-50">
           <Image
-            className="object-cover w-full h-[100vh] sm:h-[50vh] md:h-[60vh] lg:h-[70vh] xl:h-[80vh]"
+            className="object-cover w-full h-[30vh] sm:h-[40vh] md:h-[50vh] lg:h-[60vh] xl:h-[70vh] 2xl:h-[80vh]"
             src={blogBanner}
-            alt="Course Banner"
+            alt="Contact Banner"
+            priority={true} // Optional: helps load the image faster
           />
         </div>
         {/* Overlay text */}
-        <div className="container  mx-auto absolute top-1/3 sm:top-1/4 left-0 right-0 text-center">
-          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-white">
-            Blog Grid
-          </h1>
-          <div className="flex justify-center items-center gap-3 mt-5">
-            <Link className="text-lg font-bold text-white" href="/">
-              <p>Home</p>
-            </Link>
-            <FaCircleDot className="w-2 h-2 text-white" />
-            <Link className="text-lg font-bold text-white" href="/Blog">
-              <p>Blog</p>
-            </Link>
-          </div>
-        </div>
       </div>
     </>
   );
--- src/components/Course/CourseBanner.jsx
+++ src/components/Course/CourseBanner.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import CourseBanner1 from "../../image/course/page-title.fc72743c.jpg";
+import CourseBanner1 from "../../image/course/contact with us (2).png";
 import Image from "next/image";
-import Link from "next/link";
-import { FaCircleDot } from "react-icons/fa6";
 
 const CourseBanner = () => {
   return (
@@ -11,26 +9,13 @@ const CourseBanner = () => {
         {/* Image container */}
         <div className="w-full z-50">
           <Image
-            className="object-cover w-full h-[60vh] sm:h-[50vh] md:h-[60vh] lg:h-[70vh] xl:h-[80vh]"
+            className="object-cover w-full h-[40vh] sm:h-[50vh] md:h-[60vh] lg:h-[70vh] xl:h-[80vh]"
             src={CourseBanner1}
-            alt="Course Banner"
+            alt="Contact Banner"
+            priority={true} // Optional: helps load the image faster
           />
         </div>
         {/* Overlay text */}
-        <div className="container  mx-auto absolute top-1/3 sm:top-1/4 left-0 right-0 text-center">
-          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-white">
-            Our Course
-          </h1>
-          <div className="flex justify-center items-center gap-3 mt-5">
-            <Link className="text-lg font-bold text-white" href="/">
-              <p>Home</p>
-            </Link>
-            <FaCircleDot className="w-2 h-2 text-white" />
-            <Link className="text-lg font-bold text-white" href="/Courses">
-              <p>Course</p>
-            </Link>
-          </div>
-        </div>
       </div>
     </>
   );
